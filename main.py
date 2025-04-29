657eaee6f6e0d66030e8c70608bd5aac05d768b9
srinusudheer <timmusudheer007@gamil.com>
2025-04-29 22:24:28 +0530

    removed pandas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import os
 import functions_framework
 from flask import jsonify, make_response, request
-import pandas as pd
+
 
 from utils.order_utils import validate_payload, enrich_payload, simulate_db_save
 
